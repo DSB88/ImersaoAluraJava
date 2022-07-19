8de059e48a9494e8158be47688229b75dbc0b2a0
DSB88 <diego.brandao@fatec.sp.gov.br>
2022-07-18 22:10:41 -0300

    Alterando cores

--- API/App.java
+++ API/App.java
@@ -6,7 +6,9 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
-import static java.lang.Math.round;
+/*
+Projeto Imersão Alura
+ */
 
 public class App {
 
@@ -31,19 +33,44 @@ public class App {
 //        System.out.println(listaDeFilmes.size());
 //        System.out.println(listaDeFilmes.get(0));
 
-          /*
+        /*
         Exibir e manipular os dados
          */
+        System.out.println(ANSI_ROXA+"\u2735\u2735\u2735\u2735\u2735\u2735\u2735" + ANSI_RESET
+                                + ANSI_YELLOW+"IMERSÃO "+ ANSI_RESET +ANSI_AZUL+"JAVA"+ANSI_RESET+ ANSI_VERDE+" ALURA"+ANSI_RESET
+                          +ANSI_ROXA+"\u2735\u2735\u2735\u2735\u2735\u2735\u2735" +ANSI_RESET);
         for (Map<String, String> mapFilmes : listaDeFilmes) {
             String numero = mapFilmes.get("imDbRating");
-            System.out.println("\n" + "Rank: " + mapFilmes.get("rank")
-                    + ",\n Título: " + mapFilmes.get("title")
-                    + ",\n Ano Filme: " + mapFilmes.get("year")
-                    + ",\n Classificação: " + mapFilmes.get("imDbRating") +" "+ convString(numero)
-                    + " \n Poster Filme: " + mapFilmes.get("image")
+            System.out.println("\n" + ANSI_VERDE+"Rank: " +ANSI_RESET+ mapFilmes.get("rank")
+                    + ",\n" + ANSI_AZUL +" Título: "+ANSI_RESET+ mapFilmes.get("title")
+                    + ",\n" + ANSI_ROXA +" Ano Filme: " +ANSI_RESET+  mapFilmes.get("year")
+                    + ",\n" + ANSI_CIANO +" Classificação: " +ANSI_RESET+ mapFilmes.get("imDbRating") +" "+ANSI_YELLOW +convString(numero)+ANSI_RESET
+                    + " \n" + ANSI_VERMELHO +" Poster Filme: " +ANSI_RESET+  mapFilmes.get("image")
+
+
             );
         }
+        System.out.println("\n"+ANSI_RED_BACKGROUND+"FIM...."+ANSI_RESET);
     }
+    /*
+    Cores Background
+     */
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_YELLOW_BACKGROUND= "\u001B[43m";
+    public static final String ANSI_CIANO_BACKGROUND= "\u001B[46m";
+
+    /*
+  Cores linecolor
+   */
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_VERMELHO = "\u001B[31m";
+    public static final String ANSI_VERDE = "\u001B[32m";
+    public static final String ANSI_AZUL = "\u001B[34m";
+    public static final String ANSI_ROXA = "\u001B[35m";
+    public static final String ANSI_CIANO = "\u001B[36m";
+
+
 
     public static String convString(String numero) {
         String estrela = "\u272F";
@@ -55,6 +82,7 @@ public class App {
         }
         return estrela;
     }
+
 }
 
 
